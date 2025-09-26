5cbeec767df85c23610bcd25175b6087b73b5d63
giottyi <giaiotti.francesco@gmail.com>
2025-09-26 15:28:17 +0200

    added crop, check shape

--- preprocessing.py
+++ preprocessing.py
@@ -6,60 +6,71 @@ import numpy as np
 from tifffile import imread
 from tqdm import tqdm
 
-import glob, time, datetime as dt
+import glob#, time, datetime as dt
 
 
 flats_dir = r'E:\OneDrive - Politecnico di Milano\NDT@DENG\RETINA\Users\Leone Di Lernia\trasfomatore\Trasformatore_110kV_4.6mA_2s\flats'
 darks_dir = r'E:\OneDrive - Politecnico di Milano\NDT@DENG\RETINA\Users\Leone Di Lernia\trasfomatore\Trasformatore_110kV_4.6mA_2s\darks'
 projs_dir = r'E:\OneDrive - Politecnico di Milano\NDT@DENG\RETINA\Users\Leone Di Lernia\\trasfomatore\Trasformatore_110kV_4.6mA_2s\projections'
 
+crop = (600,1000)
 
-def get_views(directory):
+
+def get_views(directory, crop=None):
     views_paths = sorted(glob.glob(os.path.join(directory, "*.tif")))
     if not views_paths:
         raise UserWarning("No TIFF files found in directory.")
 
     sample_view = imread(views_paths[0])
-    sample_view = np.flipud(sample_view)
-    height, width = sample_view.shape
+    if crop is not None:
+        crop_width, crop_height = crop
+        height, width = sample_view.shape
+        cx, cy = width // 2, height // 2
+        x1 = cx - crop_width // 2
+        x2 = x1 + crop_width
+        y1 = cy - crop_height // 2
+        y2 = y1 + crop_height
+        sample_view = np.flipud(sample_view)[y1:y2, x1:x2]
+    else: sample_view = np.flipud(sample_view)
     n_views = len(views_paths)
-    views_stack = np.empty((n_views, height, width), dtype=sample_view.dtype)
+    views_stack = np.empty((n_views, *sample_view.shape), dtype=sample_view.dtype)
     views_angles = np.empty(n_views, dtype=int)
 
     views_stack[0] = sample_view
     views_angles[0] = int(views_paths[0].split('.')[1].split('_')[2])
     for i, view_path in enumerate(tqdm(views_paths[1:], desc="Loading views"), start=1):
         view = imread(view_path)
-        views_stack[i] = np.flipud(view)
+        if crop is not None:
+            views_stack[i] = np.flipud(view)[y1:y2, x1:x2]
+        else: views_stack[i] = np.flipud(view)
         views_angles[i] = int(view_path.split('.')[1].split('_')[2])
     return views_angles, views_stack
 
 
-def get_flat(directory):
+def get_flat(directory, crop=None):
     """
     averages flats maintaining precision, used in minus_log correction
     """
-    _, flats_stack = get_views(directory)
+    _, flats_stack = get_views(directory, crop=crop)
     return np.median(flats_stack, axis=0).astype(flats_stack.dtype)
 
 
-def get_dark(directory):
+def get_dark(directory, crop=None):
     """
     averages darks maintaining precision, used in flat_dark correction
     """
-    return get_flat(directory)
+    return get_flat(directory, crop=crop)
 
 
-def get_data(views_path, flat_path, dark_path=None):
+def get_data(views_path, flat_path, dark_path=None, crop=None):
     """
     applies flat-dark correction and minus_log
     """
-    flat = get_flat(flat_path)
-    angles, views = get_views(views_path)
+    flat = get_flat(flat_path, crop=crop)
+    angles, views = get_views(views_path, crop=crop)
     if dark_path is None:
         dark = np.zeros_like(flat)
-    else:
-        dark = get_dark(dark_path)
+    else: dark = get_dark(dark_path, crop=crop)
 
     def correct_log(views, flat, dark, dtype=np.float32, chunk=128):
         eps = np.finfo(np.float16).tiny
@@ -82,16 +93,18 @@ def main():
     """
     currently crops dataset to reconstruct with astra
     """
-    t0 = time.perf_counter() 
-    angles, data = get_data(projs_dir, flats_dir, darks_dir)
-    elapsed = time.perf_counter() - t0
-    print(f"Finished in {elapsed:.3f} s  ({dt.timedelta(seconds=elapsed)})")
+    #t0 = time.perf_counter() 
+    angles, data = get_data(projs_dir, flats_dir, darks_dir, crop=None)
+    print(data.dtype, data.min(), data.max())
+    #print(data.shape)
+    #elapsed = time.perf_counter() - t0
+    #print(f"Finished in {elapsed:.3f} s  ({dt.timedelta(seconds=elapsed)})")
 
-    plt.imshow(data[0], cmap='seismic')
+    plt.imshow(data[180], cmap='gray')
     plt.colorbar()
     plt.tight_layout()
     plt.gca().invert_yaxis()
-    #plt.savefig("./sample_projection.png", dpi=300)
+    plt.savefig("./sample_projection.png", dpi=300)
 
 
 
