6d0802969e247776f3afa006f2a15599609495d9
giottyi <giaiotti.francesco@gmail.com>
2025-10-17 13:13:20 +0200

    better roll estimate

--- src/camera_matrix.py
+++ src/camera_matrix.py
@@ -1,7 +1,5 @@
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
-from astropy.io import fits
 import tifffile
 
 import os, sys
@@ -9,12 +7,17 @@ import os, sys
 from preprocessing import get_views
 
 
-def _generate_objpoints(num_projs, roll_angle=0.0, tilt_angle=0.0, dims=(8,6)):
-    ''' work in progress
-    H, mask = cv.findHomography(objp_rotated[:,:-1], corners_subpix, cv2.RANSAC)
-    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, (2200,2750), None, None)
+def roll_matrix(theta):
+    c, s = np.cos(theta), np.sin(theta)
+    return np.array([
+        [c, -s, 0.0],
+        [+s, c, 0.0],
+        [0.0, 0.0, 1.0]
+    ])
+
+
+def generate_objpoints(num_projs, dims=(8,6), roll_angle=0.0, tilt_angle=0.0):
     '''
-    spacing = 14.22  # mm
     transz_step = +5.0  # mm
     sdd = 644  # mm
     sod_initial = 434  # mm
@@ -22,22 +25,21 @@ def _generate_objpoints(num_projs, roll_angle=0.0, tilt_angle=0.0, dims=(8,6)):
     M = sdd/sod
     rot_step = np.deg2rad(+3.0)
     yaw_initial = np.deg2rad(-15.0)
+    yaw_mtxs = np.stack([_yaw_matrix(yaw_initial + n * rot_step) 
+                         for n in range(num_projs)])
+    '''
+    spacing = 14.22  # mm
+    sod = 589  # mm
+    sdd = 679  # mm
+    M = sdd/sod
 
     total = dims[0]*dims[1]
     objp = np.zeros((num_projs, total, 3), np.float32)
     objp[:,:,:2] = np.mgrid[0:dims[0], 0:dims[1]].T.reshape(-1, 2)
-    objp *= spacing * M[:,np.newaxis,np.newaxis]
+    objp *= spacing * M
     centroid = np.mean(objp[:,:,:2], axis=1)
     objp[:,:,:2] -= centroid[:,np.newaxis,:]
     objp[:,:,1] *= -1
-
-    def _roll_matrix(theta):
-        c, s = np.cos(theta), np.sin(theta)
-        return np.array([
-            [c, -s, 0.0],
-            [+s, c, 0.0],
-            [0.0, 0.0, 1.0]
-        ])
 
     def _yaw_matrix(theta):
         c, s = np.cos(theta), np.sin(theta)
@@ -48,31 +50,26 @@ def _generate_objpoints(num_projs, roll_angle=0.0, tilt_angle=0.0, dims=(8,6)):
         ])
 
     def _tilt_matrix(theta):
-        c, s = np.cod(theta), np.sin(theta)
+        c, s = np.cos(theta), np.sin(theta)
         return np.array([
             [1.0, 0.0, 0.0],
             [0.0, c, -s],
             [0.0, +s, c]
         ])
 
-    roll_mtx = _roll_matrix(roll_angle)
-    tilt_mtx = _tilt_matrix(tilt_angle)
-    yaw_mtxs = np.stack([_yaw_matrix(yaw_initial + n * rot_step) 
-                         for n in range(num_projs)])
-    return roll_mtx @ objp
-    #return np.broadcast_to(objp, (num_projs,total,3)) '''
+    roll_mtx = roll_matrix(roll_angle)
+    return objp.squeeze() @ roll_mtx.T
 
 
-def _get_imgpoints(path, dims_list=[(8,6)]):
+def get_imgpoints(path, dims_list=[(10,8)]):
     _, views = get_views(path)
-    views[:,:200,:] = 0
-    views[:,:,:150] = 0
     blackwhite = np.where(views < 2000, 255, 0).astype(np.uint8)
 
     imgpoints = []
     for i, bw in enumerate(blackwhite):
         bw = cv.medianBlur(bw, 5)
         if os.environ.get("VIZ") == "2":
+            import matplotlib.pyplot as plt
             plt.imshow(bw, cmap='gray')
             plt.colorbar()
             plt.gca().invert_yaxis()
@@ -97,19 +94,56 @@ def _get_imgpoints(path, dims_list=[(8,6)]):
             print(f'No grid found in view {i}')
     if os.environ.get("VIZ") == "1":
         print(imgpoints)
-    return dims, imgpoints
+    return dims, np.array(imgpoints).squeeze().squeeze()
+
+
+def estimate_roll_2d(srcpoints, imgpoints):
+    """
+    Estimate roll angle (in radians) that best aligns srcpoints -> imgpoints
+    via least squares rotation fit.
+    """
+    src_c = srcpoints
+    dst_c = imgpoints - imgpoints.mean(axis=0)
+
+    # Compute 2×2 covariance
+    cov = src_c.T @ dst_c
+
+    # SVD for optimal rotation
+    U, _, Vt = np.linalg.svd(cov)
+    R = Vt.T @ U.T
+
+    # Fix improper rotation (reflection)
+    if np.linalg.det(R) < 0:
+        Vt[-1, :] *= -1
+        R = Vt.T @ U.T
+
+    # Roll angle around Z (in-plane)
+    roll = np.arctan2(R[1, 0], R[0, 0])
+    return roll, R
 
 
 
 if __name__ == "__main__":
+    ''' work in progress
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, (2200,2750), None, None)
+    '''
     if len(sys.argv) < 2:
         print("Usage: python camera_matrix.py <dir_with_tiffs>")
         sys.exit(1)
 
     dir_path = sys.argv[1]
-    roll_angle = np.deg2rad(+1.0)
 
-    dims, imgpoints = _get_imgpoints(dir_path)
-    objpoints = _generate_objpoints(len(imgpoints), roll_angle, dims)
-    print(objpoints)
+    dims, imgpoints = get_imgpoints(dir_path)
+    srcpoints = generate_objpoints(1, dims)[:,:2]
+
+    roll, R = estimate_roll_2d(srcpoints, imgpoints)
+    my_roll = 180 - np.rad2deg(roll)  # deg
+
+    my_R = roll_matrix(-np.deg2rad(my_roll))
+    H, mask = cv.findHomography(srcpoints @ my_R[:2,:2].T, imgpoints, cv.RANSAC)
+    np.save("../data/H_mtx_negative", H)
+
+    my_R = roll_matrix(np.deg2rad(my_roll))
+    H, mask = cv.findHomography(srcpoints @ my_R[:2,:2].T, imgpoints, cv.RANSAC)
+    np.save("../data/H_mtx_positive", H)
 
